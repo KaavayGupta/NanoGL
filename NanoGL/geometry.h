e9874101b36a60afee5f12ae75ff8d15805250a9
Kaavay Gupta <kaavaygupta@gmail.com>
2023-12-23 17:03:43 +0530

    Fix bugs in geometry.h and add cross product

--- NanoGL/geometry.h
+++ NanoGL/geometry.h
@@ -14,10 +14,12 @@ struct Vec2
 
 	Vec2(): x(0), y(0) {}
 	Vec2(T _x, T _y) : x(_x), y(_y) {}
-	inline Vec2<T> operator+(const Vec2<T>& b) const { return Vec2(x + b.x, y + b.y); }
-	inline Vec2<T> operator-(const Vec2<T>& b) const { return Vec2(x - b.x, y - b.y); }
-	inline Vec2<T> operator*(float f) const { return Vec2(x*f, y*f); }
-	inline Vec2<T> operator/(float f) const { return Vec2(x/f, y/f); }
+	inline T& operator[](const int i) { return Raw[i]; }
+	inline T operator[](const int i) const { return Raw[i]; }
+	inline Vec2<T> operator+(const Vec2<T>& b) const { return Vec2<T>(x + b.x, y + b.y); }
+	inline Vec2<T> operator-(const Vec2<T>& b) const { return Vec2<T>(x - b.x, y - b.y); }
+	inline Vec2<T> operator*(float f) const { return Vec2<T>(x*f, y*f); }
+	inline Vec2<T> operator/(float f) const { return Vec2<T>(x/f, y/f); }
 };
 
 template <class T>
@@ -32,14 +34,17 @@ struct Vec3
 
 	Vec3() : x(0), y(0), z(0) {}
 	Vec3(T _x, T _y, T _z) : x(_x), y(_y), z(_z) {}
-	inline Vec3<T> operator+(const Vec3<T>& b) const { return Vec2(x + b.x, y + b.y, z + b.z); }
-	inline Vec3<T> operator-(const Vec3<T>& b) const { return Vec2(x - b.x, y - b.y, z - b.z); }
-	inline Vec3<T> operator*(float f) const { return Vec3(x*f, y*f, z*f); }
-	inline Vec3<T> operator/(float f) const { return Vec3(x/f, y/f, z/f); }
+	inline T& operator[](const int i) { return Raw[i]; }
+	inline T operator[](const int i) const { return Raw[i]; }
+	inline Vec3<T> operator+(const Vec3<T>& b) const { return Vec3<T>(x + b.x, y + b.y, z + b.z); }
+	inline Vec3<T> operator-(const Vec3<T>& b) const { return Vec3<T>(x - b.x, y - b.y, z - b.z); }
+	inline Vec3<T> operator*(float f) const { return Vec3<T>(x*f, y*f, z*f); }
+	inline Vec3<T> operator/(float f) const { return Vec3<T>(x/f, y/f, z/f); }
 	inline T operator *(const Vec3<T>& v) const { return x * v.x + y * v.y + z * v.z; }
+	inline Vec3<T> operator ^(const Vec3<T>& v) const { return Vec3<T>(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x); }
 	float Magnitude() const { return std::sqrt(SqrMagnitude()); }
 	float SqrMagnitude() const { return (float)(x * x + y * y + z * z); }
-	Vec3<T>& Normalize() { *this = (*this) / Magnitude(); return this; }
+	Vec3<T>& Normalize() { *this = (*this) / Magnitude(); return *this; }
 	
 };
 
