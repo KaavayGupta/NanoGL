65d8767e1c4fa9887dbc317ff22ce78bed152d90
Kaavay Gupta <kaavaygupta@gmail.com>
2023-12-28 21:19:09 +0530

    Gourard Shader

--- NanoGL/main.cpp
+++ NanoGL/main.cpp
@@ -17,17 +17,46 @@ Vec3f up(0, 1, 0);
 
 struct GourardShader : public IShader
 {
-	Vec3f varying_intensity; // written by vertex shader, read by fragment shader
+	Vec3f varyingIntensity; // written by vertex shader, read by fragment shader
 
-	virtual Vec4f Vertex(int iface, int nthvert) {
-		varying_intensity[nthvert] = std::max(0.f, model->GetNormal(iface, nthvert) * lightDir); // get diffuse lighting intensity
+	virtual Vec4f Vertex(int iface, int nthvert) 
+	{
+		varyingIntensity[nthvert] = std::max(0.f, model->GetNormal(iface, nthvert) * lightDir); // get diffuse lighting intensity
+		Vec4f gl_Vertex = Embed<4>(model->GetVert(iface, nthvert)); // read the vertex from .obj file
+		return Viewport * Projection * ModelView * gl_Vertex; // transform it to screen coordinates
+	}
+
+	virtual bool Fragment(Vec3f bar, TGAColor& color) 
+	{
+		float intensity = varyingIntensity * bar;   // interpolate intensity for the current pixel
+		color = TGAColor(255, 255, 255);
+		color.R *= intensity; color.G *= intensity; color.B *= intensity;
+		return false;                              // no, we do not discard this pixel
+	}
+};
+
+struct GourardShader2 : public IShader
+{
+	Vec3f varyingIntensity; // written by vertex shader, read by fragment shader
+
+	virtual Vec4f Vertex(int iface, int nthvert) 
+	{
+		varyingIntensity[nthvert] = std::max(0.f, model->GetNormal(iface, nthvert) * lightDir); // get diffuse lighting intensity
 		Vec4f gl_Vertex = Embed<4>(model->GetVert(iface, nthvert)); // read the vertex from .obj file
 		return Viewport * Projection * ModelView * gl_Vertex; // transform it to screen coordinates
 	}
 
 	virtual bool Fragment(Vec3f bar, TGAColor& color) {
-		float intensity = varying_intensity * bar;   // interpolate intensity for the current pixel
-		color = TGAColor(255, 255, 255); // well duh
+		float intensity = varyingIntensity * bar;   // interpolate intensity for the current pixel
+		
+		if (intensity > .85) intensity = 1;
+		else if (intensity > .60) intensity = .80;
+		else if (intensity > .45) intensity = .60;
+		else if (intensity > .30) intensity = .45;
+		else if (intensity > .15) intensity = .30;
+		else intensity = 0;
+
+		color = TGAColor(50, 0, 255);
 		color.R *= intensity; color.G *= intensity; color.B *= intensity;
 		return false;                              // no, we do not discard this pixel
 	}
@@ -45,7 +74,7 @@ int main()
 	TGAImage image(width, height, 3);
 	TGAImage zbuffer(width, height, 1);
 
-	GourardShader shader;
+	GourardShader2 shader;
 	for (int i = 0; i < model->nFaces(); i++)
 	{
 		Vec4f screenCoords[3];
@@ -58,7 +87,7 @@ int main()
 
 	image.FlipVertical();
 	zbuffer.FlipVertical();
-	image.WriteTGAImage("resultsTGA/gourard_shader.tga");
+	image.WriteTGAImage("resultsTGA/gourard_shader2.tga");
 	zbuffer.WriteTGAImage("resultsTGA/gourard_shader_zbuf.tga");
 
 	delete model;
